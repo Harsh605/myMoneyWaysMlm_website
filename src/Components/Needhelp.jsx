1dabf0b2e73d56f26381e4cf8986bf379eb8fd0b
harsh <harshguptag1999@gmail.com>
2024-02-25 14:14:25 +0530

    add link

--- src/Components/Needhelp.jsx
+++ src/Components/Needhelp.jsx
@@ -7,7 +7,7 @@ const Needhelp = () => {
             <div className="Need-help-container">
                 <h1>Need help with using the platform?</h1>
                 <p>Get qualified support from MyMoneyWays experts via online chat</p>
-                <button>Contact Support</button>
+                <button><a style={{textDecoration:"none"}} href="mailto:info@mymoneyways.com">Contact Support</a></button>
 
             </div>
         </>
